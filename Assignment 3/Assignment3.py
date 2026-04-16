adaf09f014b760e9f7f5e6b946c0dd446544f8a9
Liza Mohamad Ali Aziz <lizamaa@kth.se>
2026-04-16 17:26:22 +0200

    add: exercise 3

--- Assignment 3/Assignment3.py
+++ Assignment 3/Assignment3.py
@@ -1,4 +1,6 @@
 import numpy as np
+from torch_gradient_computations_3 import ComputeGradsWithTorch
+
 # !!!
 # EXERCISE 1
 # !!!
@@ -6,9 +8,20 @@ import numpy as np
 # Loading debugging data
 debug_file = 'Assignment 3/debug_info.npz'
 load_data = np.load(debug_file)
+X1 = load_data['X1']
+P = load_data['P']
 X = load_data['X']
+Y = load_data['Y']
+
 Fs = load_data['Fs']
+grad_Fs_flat = load_data['grad_Fs_flat']
 conv_outputs_true = load_data['conv_outputs']
+conv_flat = load_data['conv_flat']
+
+W1 = load_data['W1']
+W2 = load_data['W2']
+b1 = load_data['b1']
+b2 = load_data['b2']
 
 n = X.shape[1] 
 f = Fs.shape[0]    
@@ -46,7 +59,7 @@ for i in range(n):
 difference = np.sum(np.abs(conv_outputs - conv_outputs_true))
 print(f"Error: {difference}")
 
-n_p = np_dim**2
+n_p = int(np_dim**2)
 MX = np.zeros((n_p, f**2 * 3, n))
 
 for i in range(n):
@@ -80,4 +93,121 @@ print(f"Error (matmul vs. loops): {diff_matmul}")
 conv_outputs_einsum = np.einsum('ijn, jl ->iln', MX, Fs_flat, optimize=True)
 diff_einsum = np.sum(np.abs(conv_outputs_einsum - conv_outputs_mat))
 print(f"Error (einsum vs. matMul): {diff_einsum}")
+
+# !!!
+# EXERCISE 2
+# !!!
+
+def ForwardPass(MX, network):
+    W1 = network["W"][0]
+    W2 = network["W"][1]
+    b1 = network["b"][0]
+    b2 = network["b"][1]
+    Fs_flat = network["F"]
+
+    bf = network.get("bf", np.zeros((Fs_flat.shape[1], 1)))
+
+    n = MX.shape[2]
+    n_p = MX.shape[0]
+    nf = Fs_flat.shape[1]
+
+    # using einsum and flattening the convolution output
+    conv_outputs_einsum = np.einsum('ijn, jl ->iln', MX, Fs_flat, optimize=True)
+    conv_outputs_einsum += bf.reshape((1, nf, 1))
+    # Eq. (2)
+    h = np.fmax(conv_outputs_einsum.reshape((n_p*nf, n), order='C'), 0) # conv_flat = h
+
+    # Eq. (3) and (4)
+    x1 = np.maximum(0, np.dot(W1, h) + b1)
+    s = np.dot(W2, x1) + b2
+
+    # softmax algo. Eq. (5)
+    exp_val = np.exp(s)
+    exp_val_sum = np.sum(exp_val, axis=0)
+    P = exp_val / exp_val_sum
+
+    fp_data = {"conv_flat": h, "X1": x1, "P": P}
+
+    return P, fp_data
+
+def BackwardPass(MX, Y, fp_data, network, lam):
+    W1 = network["W"][0]
+    W2 = network["W"][1]
+    Fs_flat = network["F"]
+    conv_flat = fp_data["conv_flat"]
+    P = fp_data["P"]
+    x1 = fp_data["X1"]
+
+    n = MX.shape[2]
+    n_p = MX.shape[0]
+    nf = Fs_flat.shape[1]
+
+    # From LECTURE 4
+    G_batch = P - Y
+    grad_W2 = 1/n * np.dot(G_batch, x1.T) + 2 * lam * W2
+    grad_b2 = 1/n * np.sum(G_batch, axis=1, keepdims=True)
+
+    # Backpropogate to first layer
+    G_batch = np.dot(W2.T, G_batch)
+    G_batch = G_batch * (x1 > 0) #ReLu
+
+    grad_W1 = 1/n * np.dot(G_batch, conv_flat.T) + 2 * lam * W1
+    grad_b1 = 1/n * np.sum(G_batch, axis=1, keepdims=True)
+
+    # Propogate back to convolutional layer
+    G_batch = np.dot(W1.T, G_batch)
+    G_batch = G_batch * (conv_flat > 0)
+
+    GG = G_batch.reshape((n_p, nf, n), order='C')
+
+    grad_bf = 1/n * np.sum(GG, axis=(0, 2)).reshape((nf, 1))
+
+    # # Eq. (22)
+    MXt = np.transpose(MX, (1, 0, 2))
+    grad_Fs_flat = 1/n * np.einsum('ijn, jln ->il', MXt, GG, optimize=True) + 2 * lam * Fs_flat
+
+    grads = {"W": [grad_W1, grad_W2], "b": [grad_b1, grad_b2], "F": grad_Fs_flat, "bf": grad_bf}
+
+    return grads
+
+# DEBUGGING FORWARD PASS
+# need to be in dictionary since that is the parameter format for the function
+bf = np.zeros((nf, 1)) # nf entries
+network = {"W": [W1, W2], "b": [b1, b2], "F": Fs_flat, "bf": bf}
+P_debug, fp_data_debug = ForwardPass(MX, network)
+
+diff_conv = np.sum(np.abs(fp_data_debug["conv_flat"] - conv_flat))
+diff_X1 = np.sum(np.abs(fp_data_debug["X1"] - X1))
+diff_P = np.sum(np.abs(P_debug - P))
+
+print(f"Error in conv_flat: {diff_conv}")
+print(f"Error in x1: {diff_X1}")
+print(f"Error in P: {diff_P}")
+
+"""
+# DEBUGGING BACKWARD PASS
+grads_debug = BackwardPass(MX, Y, fp_data_debug, network)
+
+diff_grad_F = np.sum(np.abs(grads_debug["F"] - grad_Fs_flat))
+
+print(f"Error in filter grads: {diff_grad_F}")
+"""
+
+# SANITY CHECK WITH TORCH
+# First 5 images
+MX_batch = MX[:, :, :5]
+Y_batch = load_data['Y'][:, :5]
+y_batch = load_data['y'][:5] 
+network["bf"] = np.random.randn(nf, 1) * 0.1
+lam = 0.01
+
+P_batch, fp_data_batch = ForwardPass(MX_batch, network)
+my_grads = BackwardPass(MX_batch, Y_batch, fp_data_batch, network, lam)
+torch_grads = ComputeGradsWithTorch(MX_batch, y_batch, network, lam)
+
+print("bf diff:", np.max(np.abs(torch_grads['bf'] - my_grads['bf'])))
+print("W1 diff:", np.max(np.abs(torch_grads['W'][0] - my_grads['W'][0])))
+print("W2 diff:", np.max(np.abs(torch_grads['W'][1] - my_grads['W'][1])))
+print("F diff:", np.max(np.abs(torch_grads['F'] - my_grads['F'])))
+
 
--- Assignment 3/torch_gradient_computations_3.py
+++ Assignment 3/torch_gradient_computations_3.py
@@ -1,27 +1,46 @@
 import numpy as np
 import torch
 
-def ComputeGradsWithTorch(X, y, network_params, lam):
+def ComputeGradsWithTorch(MX, y, network_params, lam):
     
-    Xt = torch.from_numpy(X)
+    MXt = torch.tensor(MX, dtype=torch.float32)
 
     L = len(network_params['W'])
 
     # will be computing the gradient w.r.t. these parameters    
     W = [None] * L
     b = [None] * L    
+    # Add dtype=torch.float32 to all of these!
     for i in range(len(network_params['W'])):
-        W[i] = torch.tensor(network_params['W'][i], requires_grad=True)
-        b[i] = torch.tensor(network_params['b'][i], requires_grad=True)        
+        W[i] = torch.tensor(network_params['W'][i], requires_grad=True, dtype=torch.float32)
+        b[i] = torch.tensor(network_params['b'][i], requires_grad=True, dtype=torch.float32)   
+
+    F_t = torch.tensor(network_params['F'], requires_grad=True, dtype=torch.float32)    
 
     ## give informative names to these torch classes        
     apply_relu = torch.nn.ReLU()
     apply_softmax = torch.nn.Softmax(dim=0)
 
+    n_p = MX.shape[0]
+    n = MX.shape[2]
+    nf = F_t.shape[1]
+    bft = torch.tensor(network_params['bf'], requires_grad=True, dtype=torch.float32)
+
     #### BEGIN your code ###########################
-    s1 = torch.matmul(W[0], Xt) + b[0]
-    H = apply_relu(s1)
-    scores = torch.matmul(W[1], H) + b[1]
+    conv_outputs = torch.zeros((n_p, nf, n), dtype=torch.float32)
+    for i in range(n):
+        conv_outputs[:, :, i] = torch.matmul(MXt[:, :, i], F_t)
+
+    conv_outputs += bft.view(1, nf, 1)
+    conv_flat = conv_outputs.reshape((n_p * nf, n))
+    H_conv = apply_relu(conv_flat)
+
+    # C. Fully Connected Layer 1
+    s1 = torch.matmul(W[0], H_conv) + b[0]
+    H1 = apply_relu(s1)
+    
+    # D. Fully Connected Layer 2 (Scores)
+    scores = torch.matmul(W[1], H1) + b[1]
     
     # Apply the scoring function corresponding to equations (1-3) in assignment description 
     # If X is d x n then the final scores torch array should have size 10 x n 
@@ -32,12 +51,11 @@ def ComputeGradsWithTorch(X, y, network_params, lam):
     P = apply_softmax(scores)
     
     # compute the loss
-    n = X.shape[1]
     loss = torch.mean(-torch.log(P[y, np.arange(n)]))
     
-    l2_reg = 0
+    l2_reg = torch.sum(torch.pow(F_t, 2))
     for w in W:
-        l2_reg += torch.sum(torch.pow(w, 2)) # squares the elements and sums them
+        l2_reg += torch.sum(torch.pow(w, 2))
         
     cost = loss + lam * l2_reg
     cost.backward()
@@ -49,5 +67,8 @@ def ComputeGradsWithTorch(X, y, network_params, lam):
     for i in range(L):
         grads['W'][i] = W[i].grad.numpy()
         grads['b'][i] = b[i].grad.numpy()
+
+    grads['F'] = F_t.grad.numpy()
+    grads['bf'] = bft.grad.numpy()
 
     return grads
